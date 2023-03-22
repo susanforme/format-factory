ccaa6b7219049ab1593bd71183d9cb4249266404
zhicheng ran <307853639@qq.com>
2023-03-22 16:34:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>home</div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+</style>
