60d9c7d99434c327d1c6c535ad110c79a00f4597
zhicheng ran <rzc307853639@gmail.com>
2023-03-30 16:56:03 +0800

    💡 注释

--- src/views/Picture/Picture.vue
+++ src/views/Picture/Picture.vue
@@ -1,12 +1,12 @@
 <!--
  * @Author: zhicheng ran
  * @Date: 2023-03-23 13:59:49
- * @LastEditTime: 2023-03-23 14:02:14
- * @FilePath: \format-factory\src\views\Picture.vue
+ * @LastEditTime: 2023-03-30 16:55:53
+ * @FilePath: \format-factory\src\views\Picture\Picture.vue
  * @Description: 
 -->
 <script lang="ts" setup></script>
-
+<!-- https://codepen.io/huningxin/pen/gxRKdo -->
 <template>
   <div></div>
 </template>
